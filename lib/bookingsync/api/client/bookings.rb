e9f687c5656a87b7fdd55a59bc906c65f1575987
Mariusz Pietrzyk <wijet@wijet.pl>
2014-03-07 14:57:30 +0100

    Add option to specify fields to be fetched
    It's possible to fetch only some fields from the API.
    
      @api.rentals(fields: [:name, :description])
      @api.rentals(fields: :photos)
    
    [#66583404]

--- lib/bookingsync/api/client/bookings.rb
+++ lib/bookingsync/api/client/bookings.rb
@@ -3,10 +3,13 @@ module BookingSync::API
     module Bookings
       # List bookings
       #
-      # Return public future bookings for the account user is authenticated with
-      # @return [Array<Sawyer::Resource>] list of bookings
-      def bookings
-        get :bookings
+      # Return public future bookings for the account user
+      # is authenticated with.
+      # @param options [Hash] A customizable set of options.
+      # @option options [Array] fields: List of fields to be fetched.
+      # @return [Array<Sawyer::Resource>] List of bookings.
+      def bookings(options = {})
+        get :bookings, options
       end
     end
   end
--- lib/bookingsync/api/client/rentals.rb
+++ lib/bookingsync/api/client/rentals.rb
@@ -3,14 +3,18 @@ module BookingSync::API
     module Rentals
       # List rentals
       #
-      # Returns rentals for the account user is authenticated with
-      # @return [Array<Sawyer::Resource>] list of rentals
+      # Returns rentals for the account user is authenticated with.
+      # @param options [Hash] A customizable set of options.
+      # @option options [Array] fields: List of fields to be fetched.
+      # @return [Array<Sawyer::Resource>] List of rentals.
       #
       # @example Get the list of rentals for the current account
       #   rentals = @api.rentals
       #   rentals.first.name # => "Small apartment"
-      def rentals
-        get :rentals
+      # @example Get the list of rentals only with name and description for smaller response
+      #   @api.rentals(fields: [:name, :description])
+      def rentals(options = {})
+        get :rentals, options
       end
     end
   end
