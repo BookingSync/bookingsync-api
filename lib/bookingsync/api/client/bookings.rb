e6c61b72788143d7968acecd361b44e81e699881
Mariusz Pietrzyk <wijet@wijet.pl>
2014-03-18 10:44:29 +0100

    Add booking search options
    [#67385896]

--- lib/bookingsync/api/client/bookings.rb
+++ lib/bookingsync/api/client/bookings.rb
@@ -5,9 +5,19 @@ module BookingSync::API
       #
       # Return public future bookings for the account user
       # is authenticated with.
-      # @param options [Hash] A customizable set of options.
+      # @param options [Hash] A customizable set of query options.
       # @option options [Array] fields: List of fields to be fetched.
+      # @option options [Time] from: Select bookings ending after given date.
+      # @option options [Time] until: Select bookings starting before given date.
+      # @option options [Integer] months: Select bookings starting before
+      #   :from + months, if :from is blank, current time is taken.
+      # @option options [Boolean] include_canceled: If true canceled bookings
+      #   are shown, otherwise they are hidden.
+      # @option options [Array] status: Array of booking states.
+      #   If specyfied bookings with given states are shown.
       # @return [Array<Sawyer::Resource>] Array of bookings.
+      # @example
+      #   @api.bookings(months: 12, states: [:booked, :unavailable], include_canceled: true)
       def bookings(options = {})
         get :bookings, options
       end
