e2a539184b8fd937235eae849ef28cc5ac0b283f
Mariusz Pietrzyk <wijet@wijet.pl>
2014-03-25 16:59:27 +0100

    Inquiries list and create
    [#67978768]

---
+++ lib/bookingsync/api/client/inquiries.rb
@@ -0,0 +1,22 @@
+module BookingSync::API
+  class Client
+    module Inquiries
+      # List inquiries
+      #
+      # Return list of inquiries for current account.
+      # @param options [Hash] A customizable set of query options.
+      # @return [Array<Sawyer::Resource>] Array of inquiries.
+      def inquiries(options = {}, &block)
+        paginate :inquiries, options, &block
+      end
+
+      # Create a new inquiry
+      #
+      # @param options [Hash] Inquiry attributes
+      # @return <Sawyer::Resource> Newly create inquiry
+      def create_inquiry(options = {})
+        post(:inquiries, inquiries: [options]).pop
+      end
+    end
+  end
+end
