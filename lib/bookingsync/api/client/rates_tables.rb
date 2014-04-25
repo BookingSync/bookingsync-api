d1e3d48d09a20be4d66dcda05e6bb75a937f4b08
Adam Okoń <adam@okon.eu>
2014-04-25 08:12:20 +0200

    added cud utils to rates tables endpoint

--- lib/bookingsync/api/client/rates_tables.rb
+++ lib/bookingsync/api/client/rates_tables.rb
@@ -17,6 +17,37 @@ module BookingSync::API
       def rates_tables(options = {}, &block)
         paginate :rates_tables, options, &block
       end
+
+      # Create a new rates_table
+      #
+      # @param options [Hash] rates_table attributes
+      # @return <Sawyer::Resource> Newly created rates table
+      def create_rates_table(options = {})
+        post(:rates_tables, rates_tables: [options]).pop
+      end
+
+      # Edit a rates_table
+      #
+      # @param rates_table [Sawyer::Resource|Integer] rates table or
+      # ID of the rates table to be updated
+      # @param options [Hash] rates table attributes to be updated
+      # @return [Sawyer::Resource] Updated rates table on success,
+      # exception is raised otherwise
+      # @example
+      #   rates_table = @api.rates_tables.first
+      #   @api.edit_rates_table(rates_table, { name: "Lorem" }) => Sawyer::Resource
+      def edit_rates_table(rates_table, options = {})
+        put("rates_tables/#{rates_table}", rates_tables: [options]).pop
+      end
+
+      # delete a rates_table
+      #
+      # @param rates_table [Sawyer::Resource|Integer] rates table or
+      # ID of the rates table to be deleteed
+      # @return [Array] An empty Array on success, exception is raised otherwise
+      def delete_rates_table(rates_table, options = {})
+        delete "rates_tables/#{rates_table}"
+      end
     end
   end
 end
