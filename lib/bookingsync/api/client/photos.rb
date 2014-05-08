bfe8d456916f1bd471f16fb0d6c7741c46aadecc
Mariusz Pietrzyk <wijet@wijet.pl>
2014-05-08 19:17:39 +0200

    Add CRUD for photos

--- lib/bookingsync/api/client/photos.rb
+++ lib/bookingsync/api/client/photos.rb
@@ -1,3 +1,5 @@
+require "base64"
+
 module BookingSync::API
   class Client
     module Photos
@@ -16,6 +18,54 @@ module BookingSync::API
       # @see http://docs.api.bookingsync.com/reference/endpoints/photos/#list-photos
       def photos(options = {}, &block)
         paginate :photos, options, &block
+      end
+
+      # Create a photo
+      #
+      # @param rental [BookingSync::API::Resource|Integer] Rental object or ID
+      #   for which the photo will be created.
+      # @param options [Hash] Photo's attributes.
+      # @option options [String] file_path: Required file path to the image.
+      # @return [BookingSync::API::Resource] Newly created photo.
+      # @example Create a photo.
+      #   @api.create_photo(10, file_path: 'rentals/big_one.jpg')
+      # @see http://docs.api.bookingsync.com/reference/endpoints/photos/#create-a-new-photo
+      def create_photo(rental, options = {})
+        file_path = options.delete(:file_path)
+        options[:photo] = encode(file_path)
+        options[:filename] = File.basename(file_path)
+        post("rentals/#{rental}/photos", photos: [options]).pop
+      end
+
+      # Edit a photo
+      #
+      # @param photo [BookingSync::API::Resource|Integer] Photo or ID of the
+      #   photo to be updated.
+      # @param options [Hash] Photo's attributes.
+      # @return [BookingSync::API::Resource] Updated photo
+      # @see http://docs.api.bookingsync.com/reference/endpoints/photos/#update-a-photo
+      def edit_photo(photo, options = {})
+        if file_path = options.delete(:file_path)
+          options[:photo] = encode(file_path)
+          options[:filename] = File.basename(file_path)
+        end
+        put("photos/#{photo}", photos: [options]).pop
+      end
+
+      # Destroy a photo
+      #
+      # @param photo [BookingSync::API::Resource|Integer] Photo or ID of the
+      #   photo to be removed.
+      # @return [NilClass] Returns nil on success.
+      # @see http://docs.api.bookingsync.com/reference/endpoints/photos/#destroy-a-photo
+      def destroy_photo(photo)
+        delete "photos/#{photo}"
+      end
+
+      private
+
+      def encode(file_path)
+        Base64.encode64(File.read(file_path))
       end
     end
   end
--- spec/bookingsync/api/client/photos_spec.rb
+++ spec/bookingsync/api/client/photos_spec.rb
@@ -2,11 +2,51 @@ require "spec_helper"
 
 describe BookingSync::API::Client::Photos do
   let(:client) { BookingSync::API::Client.new(test_access_token) }
+  let(:rental) { BookingSync::API::Resource.new(client, id: 2) }
 
   describe ".photos", :vcr do
     it "returns photos" do
       expect(client.photos).not_to be_empty
       assert_requested :get, bs_url("photos")
+    end
+  end
+
+  describe ".create_photo", :vcr do
+    let(:attributes) do
+      {file_path: "spec/fixtures/files/test.jpg",
+        description_en: "Funny", kind: "livingroom"}
+    end
+
+    it "creates a photo" do
+      client.create_photo(rental, attributes)
+      assert_requested :post, bs_url("rentals/2/photos"),
+        body: {photos: [attributes]}.to_json
+    end
+
+    it "returns newly created photo" do
+      VCR.use_cassette('BookingSync_API_Client_Photos/_create_photo/creates_a_photo') do
+        photo = client.create_photo(rental, attributes)
+        expect(photo.kind).to eq("livingroom")
+        expect(photo.description.en).to eq("Funny")
+      end
+    end
+  end
+
+  describe ".edit_photo", :vcr do
+    it "updates photo's description" do
+      photo = client.edit_photo(23, description_en: "Not funny anymore")
+      expect(photo.description.en).to eq("Not funny anymore")
+    end
+
+    it "updates photo's image file" do
+      photo = client.edit_photo(23, file_path: "spec/fixtures/files/test.jpg")
+      expect(photo.description.en).to eq("Not funny anymore")
+    end
+  end
+
+  describe ".destroy_photo", :vcr do
+    it "destroys given photo" do
+      expect(client.destroy_photo(23)).to be_nil
     end
   end
 end
