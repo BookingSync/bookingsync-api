183657384e90fa5883aef2a7bed69f0538cee381
Mariusz Pietrzyk <wijet@wijet.pl>
2014-05-12 14:20:46 +0200

    Fix rental agreements create methods.
    Now they require rental or booking as first param.

--- spec/bookingsync/api/client/rental_agreements_spec.rb
+++ spec/bookingsync/api/client/rental_agreements_spec.rb
@@ -29,34 +29,34 @@ describe BookingSync::API::Client::RentalAgreements do
   end
 
   describe ".create_rental_agreement_for_booking", :vcr do
-    let(:booking_id) { 2 }
+    let(:booking) { BookingSync::API::Resource.new(client, id: 2) }
 
     it "creates a new rental agreement" do
-      client.create_rental_agreement_for_booking(booking_id, attributes)
-      assert_requested :post, bs_url("rental_agreements"),
-        body: { booking_id: booking_id, rental_agreements: [attributes] }.to_json
+      client.create_rental_agreement_for_booking(booking, attributes)
+      assert_requested :post, bs_url("bookings/#{booking}/rental_agreements"),
+        body: {rental_agreements: [attributes]}.to_json
     end
 
     it "returns newly created rental agreement" do
       VCR.use_cassette('BookingSync_API_Client_RentalAgreements/_create_rental_agreement_for_booking/creates_a_new_rental_agreement') do
-        rental_agreement = client.create_rental_agreement_for_booking(booking_id, attributes)
+        rental_agreement = client.create_rental_agreement_for_booking(booking, attributes)
         expect(rental_agreement.body).to eql(attributes[:body])
       end
     end
   end
 
   describe ".create_rental_agreement_for_rental", :vcr do
-    let(:rental_id) { 20 }
+    let(:rental) { BookingSync::API::Resource.new(client, id: 20) }
 
     it "creates a new rental agreement" do
-      client.create_rental_agreement_for_rental(rental_id, attributes)
-      assert_requested :post, bs_url("rental_agreements"),
-        body: { rental_id: rental_id, rental_agreements: [attributes] }.to_json
+      client.create_rental_agreement_for_rental(rental, attributes)
+      assert_requested :post, bs_url("rentals/20/rental_agreements"),
+        body: {rental_agreements: [attributes]}.to_json
     end
 
     it "returns newly created rental agreement" do
       VCR.use_cassette('BookingSync_API_Client_RentalAgreements/_create_rental_agreement_for_rental/creates_a_new_rental_agreement') do
-        rental_agreement = client.create_rental_agreement_for_rental(rental_id, attributes)
+        rental_agreement = client.create_rental_agreement_for_rental(rental, attributes)
         expect(rental_agreement.body).to eql(attributes[:body])
       end
     end
