34a56ffd77f006b971ec232f5e6eb99ad24953a6
StoneFrog <marchelmateusz@gmail.com>
2016-12-12 09:45:54 +0100

    add more refreshed tapes

--- spec/bookingsync/api/client/rental_cancelation_policies_spec.rb
+++ spec/bookingsync/api/client/rental_cancelation_policies_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 describe BookingSync::API::Client::RentalCancelationPolicies do
   let(:client) { BookingSync::API::Client.new(test_access_token) }
 
+  before { |ex| @casette_base_path = casette_path(casette_dir, ex.metadata) }
+
   describe ".rental_cancelation_policies", :vcr do
     it "returns rental cancelation policies" do
       expect(client.rental_cancelation_policies).not_to be_empty
@@ -11,8 +13,13 @@ describe BookingSync::API::Client::RentalCancelationPolicies do
   end
 
   describe ".rental_cancelation_policy", :vcr do
+    let(:prefetched_rental_cancelation_policy_id) {
+      find_resource("#{@casette_base_path}_rental_cancelation_policies/returns_rental_cancelation_policies.yml",
+        "rental_cancelation_policies")[:id]
+    }
+
     it "returns a single rental cancelation policy" do
-      expect(client.rental_cancelation_policy(13).body).to eq "``` body ``` "
+      expect(client.rental_cancelation_policy(prefetched_rental_cancelation_policy_id).body).to eq "```body```"
     end
   end
 end
--- spec/bookingsync/api/client/rental_cancelation_policy_items_spec.rb
+++ spec/bookingsync/api/client/rental_cancelation_policy_items_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 describe BookingSync::API::Client::RentalCancelationPolicyItems do
   let(:client) { BookingSync::API::Client.new(test_access_token) }
 
+  before { |ex| @casette_base_path = casette_path(casette_dir, ex.metadata) }
+
   describe ".rental_cancelation_policy_items", :vcr do
     it "returns rental cancelation policy items" do
       expect(client.rental_cancelation_policy_items).not_to be_empty
@@ -11,8 +13,13 @@ describe BookingSync::API::Client::RentalCancelationPolicyItems do
   end
 
   describe ".rental_cancelation_policy_item", :vcr do
+    let(:prefetched_rental_cancelation_policy_item_id) {
+      find_resource("#{@casette_base_path}_rental_cancelation_policy_items/returns_rental_cancelation_policy_items.yml",
+        "rental_cancelation_policy_items")[:id]
+    }
+
     it "returns a single rental cancelation policy item" do
-      expect(client.rental_cancelation_policy_item(13).penalty_percentage).to eq "20.0"
+      expect(client.rental_cancelation_policy_item(prefetched_rental_cancelation_policy_item_id).penalty_percentage).to eq "20.0"
     end
   end
 end
